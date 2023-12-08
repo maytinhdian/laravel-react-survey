8e8a661e903b65be9d35daa75271231c2baebaf9
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-12-09 04:06:51 +0700

    Page Component 48:07

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -4,26 +4,42 @@ import Dashboard from "./views/Dashboard";
 import Login from './views/Login';
 import Signup from './views/Signup';
 import Surveys from './views/Surveys';
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 const router = createBrowserRouter([
-{
-  path:'/',
-  element:<Dashboard/>
-},
+
 {
   path:'/app',
   element:<App/>
 },
+
 {
-  path:'/surveys',
-  element:<Surveys/>
+  path:'/',
+  element:<DefaultLayout/>,
+  children:[
+    {
+      path:'/',
+      element:<Dashboard/>
+    },
+    {
+      path:'/surveys',
+      element:<Surveys/>
+    },
+  ]
 },
 {
-  path:'/login',
-  element:<Login/>
-},
-{
-  path:'/signup',
-  element:<Signup/>
-},
+  path:'/',
+  element:<GuestLayout/>,
+  children:[
+    {
+      path:'login',
+      element:<Login/>
+    },
+    {
+      path:'signup',
+      element:<Signup/>
+    },
+  ]
+}
 ])
 export default router;
