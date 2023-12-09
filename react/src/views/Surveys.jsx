b665e38c88e1ed9bfbcd36444290f9f6271666da
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2023-12-09 13:28:42 +0700

    Survey Form 1 1:43:00

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -6,22 +6,15 @@ export default function Surveys() {
   const { surveys } = useStateContext();
   console.log(surveys);
 
-  const onDeleteClick = () => {
-    console.log("On delete click");
-  };
+  const onDeleteClick = () =>{
+    console.log('On Delete Click')
+  }
 
   return (
-    <PageComponent title="Surveys"
-      // button={
-      //   <TButton color="green" to="/surveys/create">
-      //     <PlusCircleIcon className="h-6 w-6 mr-2 "></PlusCircleIcon>
-      //     Create new
-      //   </TButton>
-      // }
-    >
+    <PageComponent title="Surveys">
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
         {surveys.map((survey) => (
-          <SurveyListItem survey={survey} key={survey.id}/>
+          <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick} />
         ))}
       </div>
     </PageComponent>
