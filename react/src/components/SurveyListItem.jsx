824e41f555545e3c91a02f9e3d304c850552fea3
MinhTriet <tnhalk@gmail.com>
2023-12-09 11:06:20 +0700

    Survey Form 1

---
+++ react/src/components/SurveyListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SurveyListItem({survey}) {
+  return (
+    <div className="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-300 h-[470px]">
+
+    </div>
+  );
+}
+
+export default SurveyListItem;
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,9 +1,29 @@
 import PageComponent from "../components/PageComponent";
+import SurveyListItem from "../components/SurveyListItem";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Surveys() {
+  const { surveys } = useStateContext();
+  console.log(surveys);
+
+  const onDeleteClick = () => {
+    console.log("On delete click");
+  };
+
   return (
-    <PageComponent title='Surveys'>
-      Surveys Content
+    <PageComponent title="Surveys"
+      // button={
+      //   <TButton color="green" to="/surveys/create">
+      //     <PlusCircleIcon className="h-6 w-6 mr-2 "></PlusCircleIcon>
+      //     Create new
+      //   </TButton>
+      // }
+    >
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+        {surveys.map((survey) => (
+          <SurveyListItem survey={survey} key={survey.id}/>
+        ))}
+      </div>
     </PageComponent>
   );
 }
